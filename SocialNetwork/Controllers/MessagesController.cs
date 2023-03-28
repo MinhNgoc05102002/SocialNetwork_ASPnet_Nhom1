3f819f541b18384e6b0500b56b94ce477d5e2875
nokiddig <sy2000dn0@gmail.com>
2023-03-28 17:52:29 +0700

    send message

--- SocialNetwork/Controllers/MessagesController.cs
+++ SocialNetwork/Controllers/MessagesController.cs
@@ -2,11 +2,11 @@
 using Microsoft.EntityFrameworkCore;
 using SocialNetwork.Models;
 using SocialNetwork.ViewModels;
+using System;
 using System.Linq;
 
 namespace SocialNetwork.Controllers
 {
-    
     public class MessagesController : Controller
     {
         SocialNetworkDbContext dbContext = new SocialNetworkDbContext();
@@ -22,7 +22,6 @@ namespace SocialNetwork.Controllers
             // Nhan vao id cuoc tro chuyen, neu co thi luu id vao TempData roi chuyen sang trang ChatSession
             // de hien thi cuoc hoi thoai cu the (lam cach nay de khong hien duong dan cu the tren thanh)
             // ...
-
             return RedirectToAction("ChatSession", "Messages");
         }
 
@@ -32,18 +31,32 @@ namespace SocialNetwork.Controllers
         {
             // Kiem tra tempdata xem co luu id cua chat session khong, neu co thi render ra lich su tin nhan
             // Nhan vao id cuoc tro chuyen, chuyen toi trang message cua cuoc tro chuyen do
-            return View(CurrentAccount.Data.getListChatSession());
+            ChatSessionMessagesViewModel chatSessionMessagesViewModel = new ChatSessionMessagesViewModel(
+                CurrentAccount.Data.getListChatSession(),
+                new List<Message>(), null);
+            return View(chatSessionMessagesViewModel);
         }
 
         [HttpGet]
         [Route("Messages/ChatSession/{chatId:int}")]
         public IActionResult ChatSession(int chatId)
         {
+            Account partner = new Account();
+            foreach (Account a in GetChatMember(chatId))
+            {
+                if (a.AccountId != CurrentAccount.account.AccountId)
+                {
+                    partner = a;
+                    break;
+                }
+            }
             ChatSessionMessagesViewModel chatSessionMessagesViewModel = new ChatSessionMessagesViewModel(
                 CurrentAccount.Data.getListChatSession(),
-                dbContext.Messages.Where(x => x.ChatId == chatId).ToList());
-            // Kiem tra tempdata xem co luu id cua chat session khong, neu co thi render ra lich su tin nhan
-            // Nhan vao id cuoc tro chuyen, chuyen toi trang message cua cuoc tro chuyen do
+                dbContext.Messages.Where(x => x.ChatId == chatId).ToList(),
+                partner
+                );
+            System.Diagnostics.Debug.WriteLine(GetChatMember(chatId).Count() + " " + partner.AccountId);
+            chatSessionMessagesViewModel.chatID = chatId;
             return View(chatSessionMessagesViewModel);
         }
 
@@ -52,38 +65,73 @@ namespace SocialNetwork.Controllers
         public IActionResult ChatSessionAccount(int accountId)
         {
             //lấy ra tài khoản đích đang muốn nhắn tin
-            Account chatAccount = dbContext.Accounts.SingleOrDefault(x => x.AccountId == accountId);
-            foreach (var b in chatAccount.Chats.ToList())
+            Account partner = dbContext.Accounts.SingleOrDefault(x => x.AccountId == accountId);
+            foreach (var b in partner.Chats.ToList())
             {
                 foreach (var a in CurrentAccount.Data.getListChatSession())
                 {
                     if (a.ChatId == b.ChatId)
                     {
-                        RedirectToAction("ChatSession/" + a.ChatId, "Messages");
-                        return null;
+                        return ChatSession(a.ChatId);
                     }
                 }
             }
+            CreateNewChatSession(CurrentAccount.account, partner);
 
-            CreateNewChatSession(CurrentAccount.account, chatAccount);
-            // Kiem tra tempdata xem co luu id cua chat session khong, neu co thi render ra lich su tin nhan
-            // Nhan vao id cuoc tro chuyen, chuyen toi trang message cua cuoc tro chuyen do
-            return Redirect(accountId.ToString());
+            return ChatSessionAccount(accountId);
         }
 
-        public void CreateNewChatSession(Account sender, Account receiver)
+        public void CreateNewChatSession(Account currAcc, Account partner)
         {
             ChatSession tmp = new ChatSession();
-            tmp.Name = sender.FullName + ", " + receiver.FullName;
+            tmp.Name = currAcc.FullName + ", " + partner.FullName;
                
             dbContext.ChatSessions.Add(tmp);
             dbContext.SaveChanges();
             
             int newChatId = dbContext.ChatSessions.Max(x => x.ChatId);
             tmp = dbContext.ChatSessions.SingleOrDefault(x => x.ChatId == newChatId);
-            sender.Chats.Add(tmp);
-            receiver.Chats.Add(tmp);
+            tmp.Accounts.Add(currAcc);
+            tmp.Accounts.Add(partner);
+            
+            currAcc.Chats.Add(tmp);
+            partner.Chats.Add(tmp);
             dbContext.SaveChanges();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(string mess, int chatID)
+        {
+            Message message = new Message();
+            message.ChatId = chatID;
+            message.MessageContent = mess;
+            message.CreateAt = DateTime.Now;
+            message.AccountId = chatID;
+
+            dbContext.Messages.Add(message);
+            dbContext.ChatSessions.SingleOrDefault(x => x.ChatId == chatID).Messages.Add(message);
+            //CurrentAccount.account.Messages.Add(message);
+            dbContext.Accounts.SingleOrDefault(x => x.AccountId == CurrentAccount.account.AccountId).Messages.Add(message);
+            
+            dbContext.SaveChanges();
+            return RedirectToAction("ChatSession", new {chatID = chatID });
+        }
+
+        [HttpGet]
+        [Route("/Messages/DeleteChatSession/{chatID}")]
+        public IActionResult DeleteChatSession(int chatID)
+        {
+            dbContext.Messages.RemoveRange(dbContext.Messages.Where(x => x.ChatId == chatID));
+            dbContext.ChatSessions.Remove(dbContext.ChatSessions.SingleOrDefault(x => x.ChatId==chatID));
+            dbContext.SaveChanges();
+            return RedirectToAction("ChatSession");
+        }
+
+        public List<Account> GetChatMember(int chatID)
+        {
+            ChatSession chat = dbContext.ChatSessions.SingleOrDefault(x => x.ChatId == chatID);
+            
+            return dbContext.ChatSessions.Where(x => x.ChatId == chatID).SelectMany(x=>x.Accounts).ToList();
         }
     }
 }
--- SocialNetwork/ViewModels/ChatSessionMessagesViewModel.cs
+++ SocialNetwork/ViewModels/ChatSessionMessagesViewModel.cs
@@ -7,12 +7,19 @@ namespace SocialNetwork.ViewModels
         SocialNetworkDbContext _context = new SocialNetworkDbContext();
         public List<ChatSession> listChatSessiton { get; set; }
         public List<Message> listMessage { get; set; }
+        public Account partner { get;set; }
+        public int chatID { get; set; }
 
-        public ChatSessionMessagesViewModel(List<ChatSession> sessions, List<Message> messages)
+        public ChatSessionMessagesViewModel(List<ChatSession> sessions, List<Message> messages, Account account)
         {
             listChatSessiton = sessions;
             listMessage = messages;
+            partner = account;
         }
 
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
