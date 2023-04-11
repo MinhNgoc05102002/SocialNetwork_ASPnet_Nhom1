0bea506cacc9e99b8f4424ad1965ea262d1dc198
lxq <lxq.2309@gmail.com>
2023-04-11 21:35:15 +0700

    fix bug

---
+++ SocialNetwork/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+using SocialNetwork.Models.Authentication;
+using X.PagedList;
+
+namespace SocialNetwork.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        SocialNetworkDbContext context = new SocialNetworkDbContext();
+        [Authentication]
+        public IActionResult Index(int? page)
+        {
+            // CurrentAccount.initSession(1);
+            int currentAccountID = (int)HttpContext.Session.GetInt32("accountId");
+            
+            Account currentAccount = context.Accounts.Find(currentAccountID);
+
+            if (currentAccount.IsAdmin == true)
+            {
+                int pageNumber = page == null || page < 1 ? 1 : page.Value;
+                int pageSise = 3;
+                var listAccount = context.Accounts.AsNoTracking().OrderBy(x => x.AccountId).ToList();
+                PagedList<Account> list = new PagedList<Account>(listAccount, pageNumber, pageSise);
+                return View(list);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authentication]
+        [HttpGet]
+        public IActionResult BanAccount(int? accountId)
+        {
+
+            //if (CurrentAccount.account.IsAdmin == true)
+            int? currentAccountID = HttpContext.Session.GetInt32("accountId");
+            Account currentAccount = context.Accounts.Find(currentAccountID);
+
+            if (currentAccount.IsAdmin == true)
+            {
+                Account accountBan = context.Accounts.SingleOrDefault(x => x.AccountId == accountId);
+                if (accountBan != null && accountBan.IsAdmin == false)
+                {
+                    accountBan.IsBanned = !accountBan.IsBanned;
+                    context.Entry(accountBan).State = EntityState.Modified;
+                    context.SaveChanges();
+                    TempData["Message"] = "Changes has been saved ";
+                    return RedirectToAction("Index");
+                }
+                TempData["Message"] = "Changes hasn't been saved !!!";
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SocialNetwork/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+using SocialNetwork.Models.Authentication;
+using System.Diagnostics;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace SocialNetwork.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IHostingEnvironment _env;
+		private readonly ILogger<HomeController> _logger;
+		private SocialNetworkDbContext context = new SocialNetworkDbContext();
+
+		public HomeController(ILogger<HomeController> logger, IHostingEnvironment _enviroment)
+		{
+			_logger = logger;
+			_env = _enviroment;
+		}
+
+		[Authentication]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		//POST OPTION
+		[HttpPost]
+		public IActionResult CreatePost(string Content, List<IFormFile> images)
+		{
+			Post post = new Post();
+			post.AccountId = CurrentAccount.account.AccountId;
+			post.Content = Content;
+			post.CreateAt = DateTime.Now;
+			post.LikeCount = 0;
+			post.CommentCount = 0;
+			post.IsDeleted = false;
+			context.Posts.Add(post);
+			context.SaveChanges();
+			foreach (var image in images)
+			{
+				if (image != null)
+				{
+					string serverMapPath = Path.Combine(_env.WebRootPath, $"images/post/{CurrentAccount.account.AccountId}/{post.PostId}");
+					string serverMapPathFile = Path.Combine(serverMapPath, image.FileName);
+					Directory.CreateDirectory(serverMapPath);
+					using (var stream = new FileStream(serverMapPathFile, FileMode.Create))
+					{
+						image.CopyTo(stream);
+					}
+					string filepath = $"/images/post/{CurrentAccount.account.AccountId}/{post.PostId}/{image.FileName}";
+					Medium medium = new Medium();
+					medium.PostId = post.PostId;
+					medium.MediaLink = filepath.ToString();
+                    if (image.ContentType.Contains("image"))
+                    {
+                        medium.MediaType = "image";
+                    }
+					else if (image.ContentType.Contains("video"))
+					{
+						medium.MediaType = "video";
+					}
+					context.Media.Add(medium);
+				}
+			}
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+        public IActionResult DeletePost(string postId)
+		{
+            Post post = context.Posts.SingleOrDefault(x => x.PostId.ToString() == postId);
+            if (post != null)// && CurrentAccount.account.AccountId == post.AccountId)
+			{
+                post.IsDeleted = true;
+                context.Entry(post).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+		[Route("~/p/{postId}")]
+		public IActionResult SinglePostDetail(int postId)
+		{
+            var singlePost = context.Posts.SingleOrDefault(x => x.PostId == postId && x.IsDeleted == false);
+			if (singlePost == null)
+			{
+				return RedirectToAction("Index");
+			}
+			var singlePostDetail = new ViewModels.PostDetailViewModel(singlePost);
+			return View(singlePostDetail);
+        }
+    }
+}
